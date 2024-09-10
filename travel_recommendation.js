7d945fa0fb0c63c260d596882ba0b81f15dea0e0
Volkan2525 <volkanumudum@outlook.com>
2024-09-10 18:09:40 -0400

    commit5

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,9 +1,21 @@
-const fetch = require('fetch');
-
 var stxt = document.getElementById("searchtext");
 var sbutton = document.getElementById("searchbutton");
 document.getElementById("searchbutton").addEventListener('click',search);
 
-function search(){;
-    console.log(stxt.value);
+async function search(){
+
+    let myRequest= ('travel_recommendation_api.json');
+    await fetch(myRequest).then((res) => {
+        
+
+        return res.json;
+        
+    }).then((res)=>{
+
+        //let jvar = JSON.stringify(res);
+        console.log(res);
+
+    }).catch((err)=>{
+        window.alert(err);
+    })
 }
