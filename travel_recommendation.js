ad43c44afed9eff8c72c8604fd8c81f7bf3ccbe3
Volkan2525 <volkanumudum@outlook.com>
2024-09-11 09:11:45 -0400

    update search2

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -16,14 +16,37 @@ async function search(){
         beach.push(res.beaches.find(b => b.name.toLowerCase().toString().includes(keyword)));
         temple.push(res.temples.find(t => t.name.toLowerCase().toString().includes(keyword)));
 
-        if(country[0])
-            console.log(country);
-        if(beach[0])
-            console.log(beach);
-        if(temple[0])
-            console.log(temple);
+        let recommendations = [];
+
+        if(country[0]){
+            let cities_ = country.map(country => country.cities)[0];
+            cities_.forEach((cities_0) => {
+                let name_ = cities_0.name;
+                let description_ = cities_0.description;
+                let imageurl_ = "";   
+                recommendations.push({"name":name_, "description":description_, "imageurl":imageurl_});
+            });
+        }
+        if(beach[0]){
+            beach.forEach((beach0) => {
+                let name_ = beach0.name;
+                let description_ = beach0.description;
+                let imageurl_ = "";   
+                recommendations.push({"name":name_, "description":description_, "imageurl":imageurl_});
+            });
+        }
+        if(temple[0]){
+            temple.forEach((temple0) => {
+                let name_ = temple0.name;
+                let description_ = temple0.description;
+                let imageurl_ = "";   
+                recommendations.push({"name":name_, "description":description_, "imageurl":imageurl_});
+            });
+        }
+        console.log(recommendations);
 
     }).catch((err)=>{
         window.alert(err);
-    })
+    });
+
 }
