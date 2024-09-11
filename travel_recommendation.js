238e48bc472c2b8413af23da23d3d9f5dcf84dfb
Volkan2525 <volkanumudum@outlook.com>
2024-09-11 15:07:40 -0400

    search2

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -14,17 +14,37 @@ async function search(){
         let beach =  (res.beaches.filter(b => (JSON.stringify(b)).toString().toLowerCase().includes(keyword)));
         let temple = (res.temples.filter(t => (JSON.stringify(t)).toString().toLowerCase().includes(keyword)));
 
+        let _cities = country.map(item => item.cities);
+        let cities0 = [];
+        _cities.forEach(element => {
+            element.forEach(item =>{
+                cities0.push(item);
+            });            
+        });
 
-        if (keyword == "country")
+        //console.log(cities0);
+
+
+        let recommendations = cities0.concat(beach,temple);
+            
+
+
+        //let rec = recommendations.map(item => item.description);
+        //console.log(recommendations);
+        document.getElementById("firstDiv").innerText = recommendations[0].description;
+        document.getElementById("firstDiv").innerText = recommendations[1].description;
+        /*if (keyword == "country"){
             console.log(country);
+            //document.getElementById("firstDiv").innerText= country.description;
+        }
         else if (keyword == "beach")        
             console.log(beach);
         else if (keyword=="temple")
             console.log(temple);
-        else {
+        else if(keyword) {
             let recommendations = country.concat(beach,temple);
             console.log(recommendations);           
-        }
+        }*/
 
     }).catch((err)=>{
         window.alert(err);
